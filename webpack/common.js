4a4579797258c001b7949f88ad7b601091a41163
chaospace <ichaospace@gmail.com>
2022-04-20 08:30:40 +0900

    [webpack] eslint-prettier&hot-reload 설정 추가

--- webpack/common.js
+++ webpack/common.js
@@ -9,102 +9,110 @@ const rootDir = fs.realpathSync(process.cwd());
 const resolvePath = path => resolve(rootDir, path);
 const sourcePath = resolvePath("src");
 const outputPath = resolvePath("dist");
-const entryFilePath = resolvePath("src/index.tsx");
+const entryFilePath = resolvePath("./index.tsx");
 const publicAssetPath = resolveApp("public");
 const indexTemplatePath = resolveApp("public/index.html");
-module.exports = {
-  entry: {
-    app: entryFilePath
-  },
-  output: {
-    clean: true, // 이전 파일 제거
-    path: outputPath,
-    publicPath: "/"
-  },
-  resolve: {
-    modules: ["node_modules"],
-    alias: {
-      "@": sourcePath,
-      process: "process/browser"
+
+function getModules(isDevelopment) {
+  return [
+    {
+      test: /\.js$/,
+      type: "javascript/auto"
     },
-    extensions: [".ts", ".tsx", ".js", ".scss", ".svg", ".json"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        type: "javascript/auto"
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: [/node_modules/],
-        use: {
-          loader: "babel-loader",
+    {
+      test: /\.tsx?$/,
+      exclude: [/node_modules/],
+      use: {
+        loader: "babel-loader",
+        options: {
+          exclude: [/node_modules/],
+          cacheDirectory: true,
+          plugins: [
+            isDevelopment && require.resolve("react-refresh/babel")
+          ].filter(Boolean)
+          //babelrc: true
+        }
+      }
+    },
+    {
+      test: /(\.module)?.(sass|s.?css)$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
           options: {
-            exclude: [/node_modules/],
-            cacheDirectory: true,
-            plugins: [
-              isDevelopment && require.resolve("react-refresh/babel")
-            ].filter(Boolean)
-            //babelrc: true
+            sourceMap: isDevelopment,
+            modules: {
+              auto: /\.module.(sass|s?css)$/,
+              exportLocalsConvention: "camelCaseOnly"
+            }
+          }
+        },
+        {
+          loader: "sass-loader",
+          options: {
+            sourceMap: isDevelopment,
+            //additionalData: "@use '@/assets/styles/config';",
+            sassOptions: {
+              outputStyle: "compressed",
+              includePaths: [nodeModules]
+            }
           }
         }
-      },
-      {
-        test: /(\.module)?.(sass|s.?css)$/,
-        use: [
-          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: isDevelopment,
-              importLoaders: 2,
-              modules: {
-                auto: /\.module.(sass|s?css)$/,
-                exportLocalsConvention: "camelCaseOnly"
-              }
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: isDevelopment,
-              //additionalData: "@use '@/assets/styles/config';",
-              sassOptions: {
-                outputStyle: "compressed",
-                includePaths: [nodeModules]
-              }
+      ]
+    },
+    {
+      type: "asset",
+      resourceQuery: /url/ // *.svg?url
+    },
+    {
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "removeViewBox",
+                  active: false
+                }
+              ]
             }
           }
-        ]
+        }
+      ]
+    },
+    {
+      test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|otf|ico)$/i,
+      type: "asset"
+    }
+  ];
+}
+
+module.exports = (_, options) => {
+  const isDevelopment = options.mode === "development";
+  return {
+    entry: {
+      app: entryFilePath
+    },
+    context: rootDir,
+    output: {
+      clean: true, // 이전 파일 제거
+      path: outputPath,
+      publicPath: "/"
+    },
+    resolve: {
+      modules: ["node_modules"],
+      alias: {
+        "@": sourcePath,
+        process: "process/browser"
       },
-      {
-        type: "asset",
-        resourceQuery: /url/ // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              svgoConfig: {
-                plugins: [
-                  {
-                    name: "removeViewBox",
-                    active: false
-                  }
-                ]
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|otf|ico)$/i,
-        type: "asset"
-      }
-    ]
-  }
+      extensions: [".ts", ".tsx", ".js", ".scss", ".svg", ".json"]
+    },
+    module: {
+      rules: getModules(isDevelopment)
+    }
+  };
 };
