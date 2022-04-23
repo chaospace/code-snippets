291f019b7c589d66b759bc5b34701f2e938f8c01
chaospace <ichaospace@gmail.com>
2022-04-23 16:04:45 +0900

    [TEST] RTL설정 및 styled-component 테스트  추가

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,3 +2,8 @@ import '@testing-library/react/dont-cleanup-after-each';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import '@testing-library/react';
+import {configure} from '@testing-library/dom';
+configure({
+  testIdAttribute: 'data-my-test-id',
+  computedStyleSupportsPseudoElements: true
+});
--- tests/styled.test.tsx
+++ tests/styled.test.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
-import styled from 'styled-components';
+import styled, {StyledComponent} from 'styled-components';
 const MyText = styled.h1`
   color: black;
   font-size: 12px;
 `;
 
+function getStyledCompStyle(Component: StyledComponent<'div', any>, index = 0) {
+  const compClass = Component?.styledComponentId;
+  const compRoots = document.getElementsByClassName(compClass);
+  //console.log('compRoots', compClass, 'compRoots[index]', compRoots[index]);
+  return global.window.getComputedStyle(compRoots[index] as Element);
+}
+
+// RTL 기본동작 테스트
 test('button-render-test', () => {
   render(<MyText as="h3">hello</MyText>);
-  expect(screen.getByText('hello')).toBeInTheDocument();
+  const button = screen.getByText('hello');
+  expect(button).toBeInTheDocument();
+});
+
+test('button-style-test', () => {
+  render(<MyText as="h3">hello</MyText>);
+  const style = getStyledCompStyle(MyText);
+  expect(style.display).toEqual('block');
+  expect(style.color).toEqual('black');
+  expect(style['font-size']).toEqual('12px');
 });
