40e7da4e991226ecf3f1f1f09e37da88f3a6d651
chaospace <ichaospace@gmail.com>
2022-04-23 16:57:50 +0900

    [StoryBook] webpack5환경 스토리북 기본 설정

---
+++ .storybook/main.js
@@ -0,0 +1,46 @@
+const {nodeModules} = require('../webpack/const');
+
+module.exports = {
+  webpackFinal: async (config, {configType}) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /(\.module)?.(sass|s.?css)$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            modules: {
+              auto: /\.module.(sass|s?css)$/,
+              exportLocalsConvention: 'camelCaseOnly'
+            }
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            //additionalData: "@use '@/assets/styles/config';",
+            sourceMap: true,
+            sassOptions: {
+              outputStyle: 'compressed',
+              includePaths: [nodeModules]
+            }
+          }
+        }
+      ]
+    });
+
+    // Return the altered config
+    return config;
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5'
+  }
+};
