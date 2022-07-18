eee9f3f396fc854b77fec605cdb53dca9d5e660a
chaospace <ichaospace@gmail.com>
2022-07-18 15:41:36 +0900

    [TEST] 전역 cleanup 설정 추가

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,8 +4,12 @@ import '@testing-library/jest-dom';
 import '@testing-library/react';
 import '@testing-library/react-hooks';
 import {configure} from '@testing-library/dom';
+import {cleanup} from '@testing-library/react';
 
 configure({
   testIdAttribute: 'data-my-test-id',
   computedStyleSupportsPseudoElements: true
 });
+
+// 모든 테스트 후 cleanup 적용
+afterEach(cleanup);
