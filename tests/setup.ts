b818f6a665c82bb13ff24cb20d8e36bb2261ac1b
chaospace <ichaospace@gmail.com>
2022-07-18 11:31:58 +0900

    [SETTING] hook테스트 라이브러리 및 환경 추가

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,6 +2,7 @@ import '@testing-library/react/dont-cleanup-after-each';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import '@testing-library/react';
+import '@testing-library/react-hooks';
 import {configure} from '@testing-library/dom';
 
 configure({
