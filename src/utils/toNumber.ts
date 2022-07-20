d4780938b4a7adaf3276bffb3f1dc7d318daecee
chaospace <ichaospace@gmail.com>
2022-07-20 10:16:08 +0900

    [UTIL] toNumber에 isNumber 적용

--- src/utils/toNumber.ts
+++ src/utils/toNumber.ts
@@ -1,11 +1,9 @@
+import isNumber from './isNumber';
 import nop from './nop';
 
 const toNumber = (value: any) => {
-  if (typeof value === 'string') {
-    const nValue = parseFloat(value);
-    return isNaN(nValue) ? nop : nValue;
-  }
-  return value ?? nop;
+  const nValue = typeof value === 'string' ? parseFloat(value) : value;
+  return isNumber(nValue) ? nValue : nop;
 };
 
 export default toNumber;
