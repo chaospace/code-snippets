0c2886c37d92d4bcd3d103c17d06194198a48128
chaospace <ichaospace@gmail.com>
2023-01-06 14:08:51 +0900

    [SB] 스토리북 deocrator 테마 프로바이더 설정 및 syntaxHighlighter 적용

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
-const {
-  nodeModules,
-  sourcePath
-} = require('../webpack/const');
+const {nodeModules, sourcePath} = require('../webpack/const');
+//import type {StorybookConfig} from '@storybook/core-common';
+//import {nodeModules, sourcePath} from "webpack/cons"
+//import source from "@types/node";
+
+console.log('sourcePath', sourcePath);
+
 module.exports = {
-  webpackFinal: async (config, {
-    configType
-  }) => {
+  webpackFinal: async (config, {configType}) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
@@ -18,33 +19,41 @@ module.exports = {
     };
     config.module.rules.push({
       test: /(\.module)?.(sass|s.?css)$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
-        options: {
-          sourceMap: true,
-          modules: {
-            auto: /\.module.(sass|s?css)$/,
-            exportLocalsConvention: 'camelCaseOnly'
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            modules: {
+              auto: /\.module.(sass|s?css)$/,
+              exportLocalsConvention: 'camelCaseOnly'
+            }
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            //additionalData: "@use '@/assets/styles/config';",
+            sourceMap: true,
+            sassOptions: {
+              outputStyle: 'compressed',
+              includePaths: [nodeModules]
+            }
           }
         }
-      }, {
-        loader: 'sass-loader',
-        options: {
-          //additionalData: "@use '@/assets/styles/config';",
-          sourceMap: true,
-          sassOptions: {
-            outputStyle: 'compressed',
-            includePaths: [nodeModules]
-          }
-        }
-      }]
+      ]
     });
 
     // Return the altered config
     return config;
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5'
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,14 @@
 //import '@/assets/styles/index.scss';
 import GlobalStyle from '@/globalStyle';
-
 import {PrismLight as SyntaxHighlighter} from 'react-syntax-highlighter';
 import scss from 'react-syntax-highlighter/dist/esm/languages/prism/scss';
 import typescript from 'react-syntax-highlighter/dist/esm/languages/prism/typescript';
+import {ThemeProvider} from 'styled-components';
+import theme from '@/theme';
+
 // Registers and enables scss language support
 SyntaxHighlighter.registerLanguage('scss', scss);
 SyntaxHighlighter.registerLanguage('typescript', typescript);
-//SyntaxHighlighter.registerLanguage('jsx', jsx);
 
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
@@ -25,7 +26,9 @@ export const decorators = [
     <>
       <GlobalStyle />
       <div style={{margin: '3em'}}>
-        <Story />
+        <ThemeProvider theme={theme}>
+          <Story />
+        </ThemeProvider>
       </div>
     </>
   )
