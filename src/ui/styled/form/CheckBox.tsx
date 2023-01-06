b6acaf8e024480d85b01d228d3444e1866c7100b
chaospace <ichaospace@gmail.com>
2023-01-06 14:05:20 +0900

    [UI] 체크박스 속성 및 타입 개선

--- src/ui/styled/form/CheckBox.tsx
+++ src/ui/styled/form/CheckBox.tsx
@@ -2,40 +2,60 @@ import React, {PropsWithChildren} from 'react';
 import styled, {StyledProps, CSSProperties} from 'styled-components';
 import {getStyleProps} from '../core';
 
-type CheckBoxProps = CSSProperties & {size?: string; border?: string; iconColor?: string};
+type CheckBoxProps = StyledProps<{
+  checkBoxSize?: string;
+  border?: string;
+  borderRadius?: string;
+  checkColor?: string;
+  backgroundColor?: string;
+  fontWeight?: string;
+  color?: string;
+}>;
 
 const Input = styled.input.attrs({type: 'checkbox'})``;
-const CheckMark = styled.i<StyledProps<CheckBoxProps>>`
-  ${({size}) => size && getStyleProps({width: size, height: size})};
+const CheckMark = styled.i<CheckBoxProps>`
+  ${({checkBoxSize}) => checkBoxSize && getStyleProps({width: checkBoxSize, height: checkBoxSize})};
   && {
-    ${({size, ...rest}) => getStyleProps(rest)};
+    ${({checkBoxSize, ...rest}) => getStyleProps(rest)};
   }
 `;
-const CheckLabel = styled.span<StyledProps<CSSProperties>>`
+const CheckLabel = styled.span<CSSProperties>`
   margin-top: 2px;
   ${props => getStyleProps(props)};
 `;
-const CheckBoxBase = styled.label<StyledProps<CheckBoxProps>>`
+const CheckBoxBase = styled.label<CheckBoxProps>`
   position: relative;
   display: flex;
   flex-wrap: wrap;
+  flex-grow: 0;
   align-items: center;
   cursor: pointer;
   ${props => props.theme.checkbox};
   ${Input}:checked ~ ${CheckMark}::after {
-    ${props => props.iconColor && getStyleProps({borderColor: props.iconColor})};
+    ${props => props.checkColor && getStyleProps({borderColor: props.checkColor})};
   }
 `;
 
-function CheckBox(props: PropsWithChildren<StyledProps<CheckBoxProps>>) {
-  const {children, size, iconColor, fontWeight, backgroundColor, border, color, ...rest} = props;
+function CheckBox(props: PropsWithChildren<CheckBoxProps>) {
+  const {
+    children,
+    checkBoxSize,
+    checkColor,
+    fontWeight,
+    backgroundColor,
+    border,
+    borderRadius,
+    color
+  } = props;
   return (
-    <CheckBoxBase {...{iconColor}}>
+    <CheckBoxBase {...{checkColor}}>
       <CheckLabel {...{fontWeight, color}}>{children}</CheckLabel>
       <Input />
-      <CheckMark {...{size, backgroundColor, border}} />
+      <CheckMark {...{checkBoxSize, backgroundColor, border, borderRadius}} />
     </CheckBoxBase>
   );
 }
-
+CheckBox.defalutProps = {
+  fontWeight: 'normal'
+};
 export default CheckBox;
