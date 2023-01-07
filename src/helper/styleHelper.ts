2a66090e296429c9611a547c5f32b59d1992bdca
chaospace <ichaospace@gmail.com>
2023-01-07 21:56:35 +0900

    [UTIL] 스타일 속성 pick 함수 추가

--- src/helper/styleHelper.ts
+++ src/helper/styleHelper.ts
@@ -2,6 +2,8 @@
  * 스타일 관련 함수 모음
  */
 
+import hasProperty from '@/utils/hasProperty';
+
 function stripUnit(value: string | number): number {
   const nValue = Number(value);
   return nValue / (nValue * 0 + 1);
@@ -10,3 +12,15 @@ function stripUnit(value: string | number): number {
 function pixelToRem(value: string | number, base = 16) {
   return `${stripUnit(value) / base}rem`;
 }
+
+function pickProps<T, K extends keyof T>(style: T, keys: K[]) {
+  const results = {};
+  for (const key of keys) {
+    if (hasProperty(style, key)) {
+      results[key.toString()] = style[key];
+    }
+  }
+  return results;
+}
+
+export {pickProps};
