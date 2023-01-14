7bd280e6c6c14d69a8f7a9976e3d6c2a9bf569ae
chaospace <ichaospace@gmail.com>
2023-01-15 08:26:09 +0900

    [UI] 에러바운더리 클래스 추가

---
+++ src/ui/boundary/ErrorBoundary.tsx
@@ -0,0 +1,50 @@
+import {Component, ErrorInfo, ReactNode} from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+  fallback?: ReactNode | ((props: ErrorBoundaryState) => ReactNode);
+}
+
+interface ErrorBoundaryState {
+  error?: Error;
+  errorInfo?: ErrorInfo;
+}
+
+interface FallbackVO extends ErrorBoundaryState {
+  reset: () => void;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {error: undefined, errorInfo: undefined};
+  }
+
+  reset() {
+    this.setState({error: undefined, errorInfo: undefined});
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    this.setState({error, errorInfo});
+  }
+
+  render(): ReactNode {
+    if (this.state.error) {
+      const resetHandler = this.reset.bind(this);
+      // fallback이 있는 경우 fallback실행
+      if (this.props.fallback !== undefined) {
+        //this.setState({error: undefined, errorInfo: undefined});
+        return typeof this.props.fallback === 'function'
+          ? (this.props.fallback as Function).call(null, {
+              ...this.state,
+              reset: () => this.reset()
+            })
+          : this.props.fallback;
+      }
+    }
+    return this.props.children;
+  }
+}
+
+export {FallbackVO};
+export default ErrorBoundary;
