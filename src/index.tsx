37c41a4e8c1001564d8992cd268125c5ef346c50
chaospace <ichaospace@gmail.com>
2023-01-15 08:27:47 +0900

    [VIEW] 에러바운더리 및 테스트 페이지 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 //import React from "react";
+import {useCallback, useRef, useState} from 'react';
 import ReactDOM from 'react-dom';
 import {createRoot} from 'react-dom/client';
 import styled from 'styled-components';
@@ -7,13 +8,42 @@ import RenderApp from './examples/RenderApp';
 import GlobalStyle from './globalStyle';
 import {Space} from './ui/space';
 import {GridBox, VBox} from './ui/styled/Box';
+import Button from './ui/styled/Button';
+import Input from './ui/styled/form/Input';
 import {Text, HeadLine} from './ui/styled/Texts';
+import ErrorPage from './views/ErrorPage';
 const message = 'hello';
 
 const Wrapper = styled.div`
   position: relative;
   color: blue;
 `;
+
+const PortalTestArea = () => {
+  const [feedbackArea, setFeedbackArea] = useState<HTMLElement | undefined>(undefined);
+  const feedbackRef = useCallback((node: HTMLElement) => {
+    if (node !== null) {
+      setFeedbackArea(node);
+      return node;
+    }
+  }, []);
+
+  return (
+    <>
+      <Input
+        label="ref테스트"
+        name="user_name"
+        className="invalid"
+        style={{
+          flexDirection: 'row',
+          borderColor: 'orange'
+        }}
+        feedbackPortalElement={feedbackArea}
+      />
+      <span style={{marginLeft: '70px'}} ref={feedbackRef}></span>
+    </>
+  );
+};
 
 const appContainer = document.getElementById('app');
 if (appContainer) {
@@ -32,6 +62,10 @@ if (appContainer) {
           <div>열3</div>
         </GridBox>
         <RenderApp />
+        <ErrorPage>
+          <Text>에러페이지는 어떻게 보이나 보자</Text>
+        </ErrorPage>
+        <PortalTestArea />
       </Wrapper>
     </>
   );
---
+++ src/views/ErrorPage.tsx
@@ -0,0 +1,63 @@
+import React, {PropsWithChildren, useCallback, useState} from 'react';
+import ErrorBoundary, {FallbackVO} from '@/ui/boundary/ErrorBoundary';
+import Button from '@/ui/styled/Button';
+
+// 에러바운더리 테스트
+const ErrorFallback = ({error, errorInfo, reset}: FallbackVO) => {
+  console.log(error, errorInfo);
+  return (
+    <>
+      <span>error-boudnary!!</span>
+      <Button
+        onClick={() => {
+          reset();
+          //feedbackHandler(false);
+        }}
+      >
+        리로드!
+      </Button>
+    </>
+  );
+};
+
+const ErorrContent = (props: any) => {
+  const [provider, setProvider] = useState([{title: 'a'}]);
+
+  const reqData = useCallback(() => {
+    console.log('props', props.hasError);
+    const path = props.hasError
+      ? 'https://jsonplaceholder.typicode.com/posts'
+      : 'https://test.typicode.com/';
+
+    fetch(path)
+      .then(res => res.json())
+      .then(data => setProvider(data))
+      .catch(e => {
+        console.log('cach--');
+        props.setError(true);
+        setProvider(e);
+      });
+  }, [props]);
+  console.log('provider', provider);
+  return (
+    <>
+      <Button onClick={reqData}>데이터 로드</Button>
+      {provider[0].title}
+    </>
+  );
+};
+
+function ErrorPage(props: PropsWithChildren<{}>) {
+  const [hasError, setError] = useState(false);
+
+  return (
+    <div>
+      <ErrorBoundary fallback={(info: FallbackVO) => <ErrorFallback {...info} />}>
+        {props.children}
+        <ErorrContent hasError={hasError} setError={setError} />
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default ErrorPage;
