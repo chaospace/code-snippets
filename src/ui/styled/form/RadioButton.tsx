039d082b9c4db28131cb0052ad6300f9131b3516
chaospace <ichaospace@gmail.com>
2023-01-15 08:23:12 +0900

    [UI] 라디오버튼 스타일 테마 분리( 속성 제어시 우선순위 문제 )

--- src/ui/styled/form/RadioButton.tsx
+++ src/ui/styled/form/RadioButton.tsx
@@ -1,52 +1,83 @@
 import React, {PropsWithChildren, InputHTMLAttributes} from 'react';
 import {pickProps} from '@/helper/styleHelper';
-import styled from 'styled-components';
+import styled, {CSSProperties} from 'styled-components';
 import {getStyleProps} from '../core';
+import {colors} from '@/theme';
 
 type RadioStyleProps = {
-  labelPosition?: string;
-  type: 'radio';
+  labelPosition?: 'left' | 'right';
 };
 type RadioButtonProps = InputHTMLAttributes<HTMLInputElement> & RadioStyleProps;
 
-const Input = styled.input.attrs({type: 'radio'})`
-  ${props => props.style && getStyleProps(props.style)};
+const Input = styled.input<CSSProperties>`
+  appearance: none;
+  margin: 0 4px;
+  padding: 0;
+  width: 1.4em;
+  height: 1.4em;
+  border-radius: 50%;
+  border: 2px solid;
+  border-color: gray;
+  background-color: white;
+  transition: 0.3s ease-out;
+  transition-property: border;
+  &:disabled {
+    background-color: ${colors.disabled};
+    border-color: ${colors.disabledDarken};
+  }
+
+  &:disabled ~ span {
+    color: ${colors.disabled};
+  }
+
+  &:checked {
+    border-width: 0.4em;
+    background-color: #ffcbcb;
+  }
+  && {
+    border-color: ${props => props.borderColor};
+    &:checked {
+      background-color: ${props => props.backgroundColor};
+    }
+  }
 `;
 
-const LabelText = styled.span`
-  ${props => props.style && getStyleProps(props.style)};
+const Container = styled.label<CSSProperties>`
+  position: relative;
+  display: flex;
+  align-items: end;
+  font-size: 16px;
+  && {
+    ${props => getStyleProps({fontSize: props.fontSize})}
+  }
 `;
 
-const RadioBase = styled.label`
-  ${props => props.theme.radioButton};
+const Label = styled.span`
+  ${props => getStyleProps(props)};
 `;
 
 function RadioButton(props: PropsWithChildren<RadioButtonProps>) {
-  const {children, style, name, value, disabled, labelPosition, ...rest} = props;
+  const {children, style, labelPosition, ...rest} = props;
 
-  const labelStyle = style && pickProps(style, ['color', 'fontWeight']);
   const containerStyle = style && pickProps(style, ['fontSize']);
+  const labelStyle = style && pickProps(style, ['color', 'fontWeight']);
   const inputStyle =
-    style &&
-    pickProps(style, [
-      'width',
-      'height',
-      'backgroundColor',
-      'border',
-      'borderWidth',
-      'borderColor'
-    ]);
-  const labelText = <LabelText style={labelStyle}>{children}</LabelText>;
+    style && pickProps(style, ['backgroundColor', 'border', 'borderWidth', 'borderColor']);
+  const LabelText = <RadioButton.Label {...labelStyle}>{children}</RadioButton.Label>;
   const testIDProps = rest['data-test-id'] && {'data-test-id': rest['data-test-id']};
   delete rest['data-test-id'];
   return (
-    <RadioBase style={containerStyle} {...testIDProps}>
-      {labelPosition === 'left' && labelText}
-      <Input name={name} value={value} disabled={disabled} style={inputStyle} {...rest} />
-      {labelPosition === 'right' && labelText}
-    </RadioBase>
+    <RadioButton.Container {...containerStyle} {...testIDProps}>
+      {labelPosition === 'left' && LabelText}
+      <RadioButton.Input type="radio" {...inputStyle} {...rest} />
+      {labelPosition === 'right' && LabelText}
+    </RadioButton.Container>
   );
 }
+
+RadioButton.Container = Container;
+RadioButton.Input = Input;
+RadioButton.Label = Label;
 
 RadioButton.defaultProps = {
   labelPosition: 'left'
