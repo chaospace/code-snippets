3bd371ba3a5d38b487f9df0e5806abe4f7dcf6cc
chaospace <ichaospace@gmail.com>
2023-02-20 09:06:00 +0900

    [설정] testing-library/react-hooks 제거( react 18버전 지원 안함 )

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,7 +2,6 @@ import '@testing-library/react/dont-cleanup-after-each';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import '@testing-library/react';
-import '@testing-library/react-hooks';
 import {configure} from '@testing-library/dom';
 import {cleanup} from '@testing-library/react';
 
